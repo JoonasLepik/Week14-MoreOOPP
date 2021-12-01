3cf09af5f7a134293b8d3ca01f960c39c09de693
Opilane TTHK <opilane@tthk.ee>
2021-12-01 10:27:08 +0200

    more OOPP

---
+++ OOPPet/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace OOPPet
+{
+    class Program
+    {
+        class Pet
+        {
+            string name;
+            int age;
+            double weight;
+            public Pet(string _name) //Constuctor
+            {
+                name = _name;
+                age = 0;
+                weight = 0.1;
+                Console.WriteLine($"Congrations! you have adopted {name}!");
+
+            }
+            // getters for class properties
+            public string Name
+            {
+                get { return name; }
+
+            }
+            public double Weight
+            {
+                get { return weight; }
+
+            }
+
+            public void ShowPetData()
+            {
+                Console.WriteLine($"Name: {name}");
+                Console.WriteLine($"Weight: {weight}");
+                Console.WriteLine($"Age: {age}");
+            }
+            public void Eat()
+            {
+                weight += 0.2; //happiness = happiness + 0.2
+            }
+            public void Exesise()
+            {
+                Console.WriteLine("The hamster is runing around it's cage.");
+                weight -= 0.1;
+            }
+            public void Rename(string newName)
+            {
+                name = newName;
+            }
+            public void GetOlder()
+            {
+                age++;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Pet myHamster = new Pet("Hamster");
+            myHamster.ShowPetData();
+            Console.WriteLine("Please enter an new name to rename your hamster:");
+            string userInput = Console.ReadLine();
+            myHamster.Rename(userInput);
+            Console.WriteLine($"Name: {myHamster.Name}");
+            for (int i = 1; i < 10; i++)
+            {
+                myHamster.Eat();
+                Console.WriteLine($"Current weight: {myHamster.Weight}");
+                while(myHamster.Weight > 0.7)
+                {
+                    myHamster.Exesise();
+                }
+
+                /*if(myHamster.Weight > 0.7)
+                {
+                    myHamster.Exesise();
+                }*/
+                myHamster.GetOlder();
+            }
+            myHamster.ShowPetData();
+            Console.WriteLine("We cant help you no longer.");
+        }    
+    }
+}
---
+++ OOPcar/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace OOPCar
+{
+    class Program
+    {
+        class Car
+        {
+            string markName;
+            string modelName;
+            string registrationNumber;
+            string color;
+            int fuelTank;
+            int odometer;
+
+            public Car(string _name, string _markName, string _color, string _regNumb) //Constuctor
+            {
+                markName = _markName;
+                modelName = _name;
+                registrationNumber = _regNumb;
+                color = _color;
+                fuelTank = 60;
+                odometer = 0;
+                Console.WriteLine($"{markName} {modelName} {registrationNumber} {color} has been created.");
+            }
+            public void ShowCarData()
+            {
+                Console.WriteLine($"MarkName: {markName}");
+                Console.WriteLine($"modelName: {modelName}");
+                Console.WriteLine($"registrationNumber: {registrationNumber}");
+                Console.WriteLine($"color: {color}");
+                Console.WriteLine($"fuel: {fuelTank}");
+                Console.WriteLine($"odometer: {odometer}");
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Toyota", "Celica", "777GRD", "orange");
+
+        }
+    }
+}
